f4f16770155d0b7c8b746677800fa98c901f711a
MrShervey <jimshervey2@gmail.com>
2022-12-06 14:15:40 +0000

    .

---
+++ Challenges/Test/live_Coding_Test.py
@@ -0,0 +1,10 @@
+# We want to create a program that:
+# Takes three integers as inputs
+# If the sum of the first and last numbers are greater than the second number then the second number is 
+# subtracted from the third
+# If not then then the sum of all three are calculated
+# Return the result
+# Multiply the result by the first number
+# Print the value calculated
+
+# Use three subroutines, and input validation
