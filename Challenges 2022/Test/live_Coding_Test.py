e94168c7dd6dfcbd07f442cafaa539e9caa6bde8
MrShervey <jimshervey2@gmail.com>
2022-12-06 22:33:39 +0000

    .

--- Challenges 2022/Test/live_Coding_Test.py
+++ Challenges 2022/Test/live_Coding_Test.py
@@ -9,3 +9,25 @@
 
 # Use three subroutines, and input validation
 
+def getInput():
+    valid = False
+    number = input("Enter a number: ")
+    while not valid:
+        try:
+            number = int(number)
+            valid = True
+        except ValueError:
+            number = input("it needs to be a number")
+    return number
+
+def calculate(num1, num2, num3):
+    if (num1 + num3) > num2:
+        return num3-num2
+    else:
+        return num1+num2+num3
+
+def final(first, result):
+    print(result*first)
+
+number1 = getInput()
+final(number1,calculate(number1,getInput(),getInput()))
