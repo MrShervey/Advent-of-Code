a72b4fa83113b1c133770aa87c75993e5afa37c4
MrShervey <jimshervey2@gmail.com>
2023-11-23 16:09:28 +0000

    1

--- Challenges 2023/1/Day 1/Program.cs
+++ Challenges 2023/1/Day 1/Program.cs
@@ -1,9 +1,40 @@
 ﻿using System;
+using System.Formats.Asn1;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
+    static List<int> getData()
+    {
+        List<int>inputData = new List<int>();
+        TextReader TextFile = new StreamReader("inputValues.txt");
+        string data;
+        while ((data = TextFile.ReadLine()) != null)
+        {
+            inputData.Add(Int32.Parse(data));
+        }
+        TextFile.Close();
+        return inputData;
+    }
     static void Main()
     {
-        Console.WriteLine("HJelllllloooooo");
+        int answer = 0;
+        List<int> values = new List<int>();
+        values = getData();
+        for(int x = 0; x < values.Count; x++)
+        {
+            for (int y = 1; y < values.Count-1; y++)
+            {
+                for (int z = 2; z < values.Count-2;z++)
+                {               
+                    if((values[x] + values[y] + values[z]) == 2020)
+                    {
+                        answer = values[x] * values[y] * values[z];
+                        break;
+                    }
+                }
+            }
+        }
+        Console.WriteLine(answer);
     }
 }
