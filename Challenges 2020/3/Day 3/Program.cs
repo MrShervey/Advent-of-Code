6c45905bb9894e2b95f9b9cc744d8ec832db56af
MrShervey <jimshervey2@gmail.com>
2023-11-30 14:55:14 +0000

    1

---
+++ Challenges 2020/3/Day 3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static List<string> getData()
+    {
+        TextReader TextFile = new StreamReader("dataSet.txt");
+        string data;
+        List<string> fullData = new List<string>();
+        while ((data = TextFile.ReadLine()) != null)
+        {
+            fullData.Add(data);
+        }
+        TextFile.Close();
+        return fullData;
+    }
+    static int taskOneChanged(int right, int down)
+    {
+        List<string> dataSet = getData();
+        int open = 0, trees = 0, row = 0, column = 0;
+        int length = dataSet[0].Length;
+        while (row < 322)
+        {
+            column += right;
+            row += down;
+            if (column > length-1)
+            {
+                column = (column - (length-1)) - 1;
+            }
+            if (Convert.ToString(dataSet[row][column]) == ".")
+            {
+                open++;
+            }
+            else
+            {
+                trees++;
+            }
+        }
+
+        return trees;
+    }
+    static void Main()
+    {
+        
+        int val1 = taskOneChanged(1,1);
+        int val2 = taskOneChanged(3,1);
+        int val3 = taskOneChanged(5,1);
+        int val4 = taskOneChanged(7,1);
+        int val5 = taskOneChanged(1,2);
+        double total = val1 * val2 * val3 * val4 * val5;
+        Console.WriteLine(total);
+        
+    }
+}
---
+++ Challenges 2020/4/Day4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static int getData()
+    {
+        int total = 0;
+        TextReader TextFile = new StreamReader("testData.txt");
+        string data = TextFile.ReadToEnd();
+        data = data.Replace(Environment.NewLine, " ");
+        List<string> fields = new List<string>();
+        string[] splitData = data.Split(" ");
+        for (int x = 0; x < splitData.Length; x++)
+        {
+            string[] section = splitData[x].Split(":");
+            fields.Add(section[0]);
+        }
+        if (fields.Count == 8)
+        {
+            total++;
+        }
+        else if (fields.Count == 7)
+        {
+            if (!fields.Contains("cid"))
+            {
+                total++;
+            }
+        }  
+        return total;         
+        }
+       
+        
+    
+    static void Main()
+    {
+        Console.WriteLine(getData());
+    }
+}
