746016c35c6a4a82822c1ff43f19cd644a58bdcd
MrShervey <jimshervey2@gmail.com>
2023-12-01 09:49:54 +0000

    1

--- Challenges 2023/1/Day One/Program.cs
+++ Challenges 2023/1/Day One/Program.cs
@@ -2,9 +2,42 @@
 
 class Program
 {
+    static List<string> getData()
+    {
+        List<string>inputData = new List<string>();
+        TextReader TextFile = new StreamReader("inputData.txt");
+        string data;
+        while ((data = TextFile.ReadLine()) != null)
+        {
+            inputData.Add(data);
+        }
+        TextFile.Close();
+        return inputData;
+    }
+    static string taskOne(string inputText)
+    {
+        string total;
+        int number;
+        List<int> numbers = new List<int>();
+        for (int x = 0; x < inputText.Length; x++)
+        {
+            if(int.TryParse(inputText[x].ToString(), out number))
+            {
+                numbers.Add(number);
+            }
+        }
+        total = numbers[0].ToString() + numbers[numbers.Count - 1].ToString();
+        return total;
+    }
 
     static void Main()
     {
-        
+        int finalTotal = 0;
+        List<string> values = getData();
+        for (int x = 0; x < values.Count; x++)
+        {
+            finalTotal += Convert.ToInt32(taskOne(values[x]));
+        }
+        Console.WriteLine(finalTotal);
     }
 }
