2bbd303f365ef8c10f0b1639374f73ffc4093101
MrShervey <jimshervey2@gmail.com>
2023-12-01 14:51:39 +0000

    1

---
+++ Challenges 2015/1/Day 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+        static string getData()
+    {
+        TextReader textFile = new StreamReader("inputData.txt");
+        string data = textFile.ReadLine();
+        textFile.Close();
+        return data;
+    }
+    static int taskOne(string data)
+    {
+        int floor = 0;
+        for (int x = 0; x < data.Length; x++)
+        {
+            if (data[x].ToString() == "(")
+            {
+                floor++;
+            }
+            else
+            {
+                floor--;
+            }
+            if (floor == -1)
+            {
+                return x+1;
+            }
+        }
+        return floor;
+    }
+    static void Main()
+    {
+        Console.WriteLine(taskOne(getData()));
+    }
+}
