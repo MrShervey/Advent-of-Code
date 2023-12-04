895ce4f5bec328e3579e83c3c5f94f78d34ede94
MrShervey <jimshervey2@gmail.com>
2023-12-04 12:12:54 +0000

    1

--- Challenges 2023/4/Day 4/Program.cs
+++ Challenges 2023/4/Day 4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 class Program
 {
     static List<string> getData()
@@ -15,10 +16,43 @@ class Program
     } 
     static int taskOne(string data)
     {
-        
+        int winCount = 0;
+        string[] splitData = data.Split(":");
+        string[] newSplit = splitData[1].Split("|");
+        string tempWin = newSplit[0];
+        string[] winNums = tempWin.Split(" ");
+        winNums= winNums.Except(new List<string> { string.Empty }).ToArray();
+        string tempNums = newSplit[1];   
+        string[] myNums = tempNums.Split(" ");
+        myNums= myNums.Except(new List<string> { string.Empty }).ToArray();
+        for (int x = 0; x < winNums.Length; x++)
+        {
+            for (int y = 0; y < myNums.Length; y++)
+            {
+                if (Convert.ToInt32(winNums[x]) == Convert.ToInt32(myNums[y]))
+                {
+                    if (winCount >= 1)
+                    {
+                        winCount*=2;
+                    }
+                    else
+                    {
+                        winCount = 1;
+                    }
+                    
+                }
+            }
+        }
+        return winCount;
     }
     static void Main()
     {
-
+        List<string> values = getData();
+        int winTotal = 0;
+        for (int x = 0; x < values.Count; x++)
+        {
+            winTotal+=taskOne(values[x]);
+        }
+        Console.WriteLine(winTotal);
     }
 }
