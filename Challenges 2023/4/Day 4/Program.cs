3946e7bfb37bd72bffc2a1da61938c57be3c9ebe
MrShervey <jimshervey2@gmail.com>
2023-12-04 16:03:53 +0000

    1

--- Challenges 2023/4/Day 4/Program.cs
+++ Challenges 2023/4/Day 4/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Data.SqlTypes;
 using System.Linq;
 class Program
 {
+    static List<string> values = getData();
+    static List<int>winsPerCard = new List<int>();
     static List<string> getData()
     {
         List<string>inputData = new List<string>();
@@ -14,17 +17,22 @@ class Program
         textFile.Close();
         return inputData;
     } 
-    static int taskOne(string data)
+    static void splitting(string data, out string[] winNums, out string[] myNums)
     {
-        int winCount = 0;
         string[] splitData = data.Split(":");
         string[] newSplit = splitData[1].Split("|");
         string tempWin = newSplit[0];
-        string[] winNums = tempWin.Split(" ");
+        winNums = tempWin.Split(" ");
         winNums= winNums.Except(new List<string> { string.Empty }).ToArray();
         string tempNums = newSplit[1];   
-        string[] myNums = tempNums.Split(" ");
+        myNums = tempNums.Split(" ");
         myNums= myNums.Except(new List<string> { string.Empty }).ToArray();
+    }
+    static int taskOne(string data)
+    {
+        int winCount = 0, cardwins = 0;
+        string[] winNums, myNums;
+        splitting(data, out winNums, out myNums);
         for (int x = 0; x < winNums.Length; x++)
         {
             for (int y = 0; y < myNums.Length; y++)
@@ -39,20 +47,43 @@ class Program
                     {
                         winCount = 1;
                     }
+                    cardwins++;
                     
                 }
             }
         }
+        winsPerCard.Add(cardwins);
         return winCount;
+    }
+    static int taskTwo()
+    {
+        int[] totalCards = new int[winsPerCard.Count];
+        int start = 0;
+        for (int x = 0; x < totalCards.Length; x++)
+        {
+            totalCards[x]++;
+            for (int y = 0; y < totalCards[x]; y++)
+            {
+                start = x+1;
+                for (int z = 0; z < winsPerCard[x]; z++)
+                {
+                    totalCards[start]++;
+                    start++;
+                }
+            }
+        }
+
+        return totalCards.Sum();
     }
     static void Main()
     {
-        List<string> values = getData();
+        
         int winTotal = 0;
         for (int x = 0; x < values.Count; x++)
         {
             winTotal+=taskOne(values[x]);
         }
-        Console.WriteLine(winTotal);
+        Console.WriteLine("Task 1: {0}",winTotal);
+        Console.WriteLine("Task 2: {0}",taskTwo());
     }
 }
