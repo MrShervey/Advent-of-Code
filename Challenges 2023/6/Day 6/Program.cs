5ccd5492add4026938ab097ed52f40893d59e327
MrShervey <jimshervey2@gmail.com>
2023-12-05 19:51:51 +0000

    1

---
+++ Challenges 2023/6/Day 6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static List<string> getData()
+    {
+        List<string>inputData = new List<string>();
+        TextReader textFile = new StreamReader("inputData.txt");
+        string data;
+        while ((data = textFile.ReadLine()) != null)
+        {
+            inputData.Add(data);
+        }
+        textFile.Close();
+        return inputData;
+    } 
+    static void Main()
+    {
+        
+    }
+}
