62b6e7fc0ca743c6acc667c36b69d1dcc00e6f36
MrShervey <jimshervey2@gmail.com>
2023-12-05 12:19:55 +0000

    1

--- Challenges 2023/5/Day 5/Program.cs
+++ Challenges 2023/5/Day 5/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 class Program
 {
     static List<string> getData()
     {
         List<string>inputData = new List<string>();
-        TextReader textFile = new StreamReader("inputData.txt");
+        TextReader textFile = new StreamReader("testData.txt");
         string data;
         while ((data = textFile.ReadLine()) != null)
         {
@@ -13,8 +14,56 @@ class Program
         textFile.Close();
         return inputData;
     } 
+    static Dictionary<int,int> createDict(List<string> data)
+    {
+        Dictionary<int,int>newDict = new Dictionary<int, int>();
+        for (int x=0;x<data.Count;x++)
+        {
+            int[]values = Array.ConvertAll(data[x].Split(" "), int.Parse);
+            int add = 0;
+            for (int y = 0; y < values[2];y++)
+            {
+                newDict.Add(values[1]+add,values[0]+add);
+                add++;
+            }
+        }
+        return newDict;
+    }
+    static List<int> checkData(List<string> data)
+    {
+        Dictionary<int,int>dictionary = new Dictionary<int, int>();
+        string[] stringSeeds = data[0].Split(" ");
+        List<int>seeds = new List<int>();
+        for(int x = 1; x<stringSeeds.Length; x++)
+        {
+            seeds.Add(Convert.ToInt32(stringSeeds[x]));
+        }
+        int start = 2;
+        for (int x=0; x<7;x++)
+        {
+            List<string>dictValues = new List<string>();
+            start++;
+            while(data[start] != "")
+            {
+                dictValues.Add(data[start]);
+                start++;
+            }
+            start++;
+            dictionary = createDict(dictValues);
+            for (int y = 0; y< seeds.Count;y++)
+            {
+                if (dictionary.ContainsKey(seeds[y]))
+                {
+                    seeds[y] = dictionary[seeds[y]];
+                }
+            }
+        }
+        return seeds;
+    }
     static void Main()
     {
-
+        List<string>inputtedData = getData();
+        List<int> finalValues= checkData(inputtedData);
+        int check = 0;
     }
 }
