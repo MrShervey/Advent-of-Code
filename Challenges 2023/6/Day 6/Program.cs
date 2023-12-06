2481a6c33e3d4e5b528253e31d8f23492d85f498
MrShervey <jimshervey2@gmail.com>
2023-12-06 10:52:19 +0000

    a

--- Challenges 2023/6/Day 6/Program.cs
+++ Challenges 2023/6/Day 6/Program.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 class Program
 {
-    static List<string> getData()
+    static long findTimes(long raceTime, long raceDistance)
     {
-        List<string>inputData = new List<string>();
-        TextReader textFile = new StreamReader("inputData.txt");
-        string data;
-        while ((data = textFile.ReadLine()) != null)
+        int races = 0;
+        for (int x = 0; x < raceTime; x++)
         {
-            inputData.Add(data);
+            if((raceTime-x)*x > raceDistance)
+            {
+                races++;
+            }
         }
-        textFile.Close();
-        return inputData;
-    } 
+        return races;
+    }
     static void Main()
     {
-        
+        //test Data
+        // int[]time = {7,15,30};
+        // int[]distance = {9,40,200};
+        long total = 1;
+        //full Data
+        int[]time = {45,98,83,73};
+        long[]distance = {295,1734,1278,1210};
+        // for (int x = 0; x < time.Length; x++)
+        // {
+        //     total *= findTimes(time[x], distance[x]);
+        // }
+        // Console.WriteLine(total);
+        //Task Two test
+        //Console.WriteLine(findTimes(71530,940200));
+        //Task Two full data
+        Console.WriteLine(findTimes(45988373,295173412781210));
     }
 }
