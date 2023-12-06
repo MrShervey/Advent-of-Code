845722dcaac7f29d25781a106b3ff032fc8bf4f1
MrShervey <jimshervey2@gmail.com>
2023-12-06 13:01:41 +0000

    a

--- Challenges 2023/6/Day 6/Program.cs
+++ Challenges 2023/6/Day 6/Program.cs
@@ -15,21 +15,26 @@ class Program
     }
     static void Main()
     {
-        //test Data
+        //Part One test Data
         // int[]time = {7,15,30};
         // int[]distance = {9,40,200};
         long total = 1;
-        //full Data
-        int[]time = {45,98,83,73};
+        //Part One full Data
+        long[]time = {45,98,83,73};
         long[]distance = {295,1734,1278,1210};
-        // for (int x = 0; x < time.Length; x++)
-        // {
-        //     total *= findTimes(time[x], distance[x]);
-        // }
-        // Console.WriteLine(total);
-        //Task Two test
-        //Console.WriteLine(findTimes(71530,940200));
-        //Task Two full data
-        Console.WriteLine(findTimes(45988373,295173412781210));
+        //Part One
+        for (int x = 0; x < time.Length; x++)
+        {
+            total *= findTimes(time[x], distance[x]);
+        }
+        Console.WriteLine("Part One: {0}",total);
+        //Part Two test data
+        // int p2Time  = 71530;
+        // int p2Distance = 940200;
+        // Console.WriteLine(findTimes(p2Time,p2Distance));
+        //Part Two full data
+        long p2Time  = 45988373;
+        long p2Distance = 295173412781210;
+        Console.WriteLine("Part Two: {0}",findTimes(p2Time,p2Distance));
     }
 }
